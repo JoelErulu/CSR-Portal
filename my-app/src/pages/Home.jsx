19dd88b6570fc1fe6ca3a9fe5589fea7f1d1ee6d
Joel Erulu <joelerulu088@gmail.com>
2026-04-13 18:45:02 -0400

    deleted placeholder information

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home(){
+
+    return (
+    <div>
+        <h1> CSR System </h1>
+        <button> Create Mock Customer Data </button>
+        <button> Customer Service Portal </button>
+    </div>
+    );
+
+}
